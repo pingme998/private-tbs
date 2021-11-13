0acf047dcabdc4596e08799bc205e96fdbbda088
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-11-13 23:40:39 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo $PORT >/PORT
 cat /default |sed "s/THERANDOMPORT/$(cat /PORT)/g" >/etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
+
+#failsafe serve
+rclone serve http CLOUDNAME: --addr :$(cat /PORT) --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
